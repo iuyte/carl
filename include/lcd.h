6840c1ed3e0fd90c329507e3fa6bc226d6f192a4
iuyte <iuyte@gmx.com>
2017-10-15 20:15:18 -0400

    Made LCD interaction easier

---
+++ include/lcd.h
@@ -0,0 +1,43 @@
+/*
+ * @file lcd.h
+ * @brief Useful for interacting with LCD displays. Menus and the like
+ * Copyright (C) 2017 Ethan Wells
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or(at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <https://www.gnu.org/licenses/>
+ */
+
+#ifndef CARL_LCD_H_
+#define CARL_LCD_H_
+
+#include "API.h"
+
+typedef struct Menu {
+	char        *text[2];
+	struct Menu *parent;
+	struct Menu *prev;
+	struct Menu *next;
+	struct Menu *(*onSelect)();
+} Menu;
+
+static Menu lcdRoot;
+
+typedef struct LCD {
+	PROS_FILE *port;
+	Menu      *currentMenu;
+} LCD;
+
+extern TaskHandle lcdManagerHandle;
+void lcdManager(void *none);
+
+#endif // CARL_LCD_H_
